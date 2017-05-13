9d61668ae187e7a3df550815444e12829a880531
dennyzhang <denny@dennyzhang.com>
2017-05-12 23:01:15 -0700

    update scripts

--- codecheck/docker_shellcheck.sh
+++ codecheck/docker_shellcheck.sh
@@ -9,26 +9,52 @@
 ## Description :
 ## --
 ## Created : <2017-05-12>
-## Updated: Time-stamp: <2017-05-12 22:49:20>
+## Updated: Time-stamp: <2017-05-12 23:01:10>
 ##-------------------------------------------------------------------
-ignore_file=${1?""}
+code_dir=${1?""}
 container_name=${2?""}
-code_dir=${3?""}
-image_name=${4:-"denny/pylint:1.0"}
+image_name=${3:-"denny/pylint:1.0"}
+ignore_file_list=${4-""}
+
+current_filename=$(basename "$0")
+ignore_file="${current_filename%.sh}_$$"
+
+function remove_container() {
+    container_name=${1?}
+    if docker ps -a | grep "$container_name" 1>/dev/null 2>&1; then
+        echo "Destroy container: $container_name"
+        docker stop "$container_name"; docker rm "$container_name"
+    fi
+}
+
+function shell_exit() {
+    errcode=$?
+    if [ $errcode -eq 0 ]; then
+        echo "Test has passed."
+    else
+        echo "ERROR: Test has failed."
+    fi
+
+    echo "Remove tmp file: $ignore_file"
+    rm -rf "$ignore_file"
+
+    remove_container "$container_name"
+    exit $errcode
+}
+
+################################################################################
+trap shell_exit SIGHUP SIGINT SIGTERM 0
 
 echo "Generate the ignore file for code check"
-echo $pylint_ignore > /tmp/$ignore_file
+echo "$ignore_file_list" > "/tmp/$ignore_file"
 
 echo "Start container"
-docker stop $container_name; docker rm $container_name; true
-docker run -t -d --privileged -v ${code_dir}:/code --name $container_name --entrypoint=/bin/sh $image_name
+remove_container "$container_name"
+docker run -t -d --privileged -v "${code_dir}:/code" --name "$container_name" --entrypoint=/bin/sh "$image_name"
 
 echo "Copy ignore file"
-docker cp /tmp/$ignore_file $container_name:/$ignore_file
+docker cp "/tmp/$ignore_file" "$container_name:/$ignore_file"
 
 echo "Run pylint check"
-docker exec -t $container_name python /enforce_pylint_check.py --code_dir /code --check_ignore_file /${ignore_file}
-
-echo "Destroy container"
-docker stop $container_name; docker rm $container_name
+docker exec -t "$container_name" python /enforce_pylint_check.py --code_dir /code --check_ignore_file "/${ignore_file}"
 ## File : docker_shellcheck.sh ends
